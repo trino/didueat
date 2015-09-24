e72386cd9509dc7b641f3f5601a1b4bdfbce1ed7
trino <minesh@trinoweb.com>
2015-09-24 17:14:40 -0400

    Roy 5:10
    Helper

--- app/Http/Functions/helpers.php
+++ app/Http/Functions/helpers.php
@@ -1,21 +1,133 @@
 <?php
 
 function test(){
-    return;
     DB::enableQueryLog();
+    return;
 
-    getColumnNames("countries");
-    $Test = getallQueries();
-
+    $Test = get_profile_type(1);
     debug($Test);
-
-
     die();
+}
+
+function logevent($Text, $IdontKNOW){
+
 }
 
 
 
 
+
+//////////////////////////////////////////////////PROFILE TYPES API/////////////////////////////////////////////////////
+function new_profiletype($Name){
+    logevent("Made a new profile type: " . $Name, false);
+    return new_anything("profiletypes", $Name);
+}
+
+function get_profile_permissions(){//lists all permissions
+    return getColumnNames("profiletypes", array("ID", "Name", "Hierarchy"));
+}
+
+function enum_profiletypes($Hierarchy = "", $toArray = true){
+    $Condition = "1=1";
+    if($Hierarchy){
+        $Condition = "Hierarchy > " . $Hierarchy;
+    }
+    $entries = enum_all("profiletypes", $Condition);
+    if($toArray) {return my_iterator_to_array($entries, "ID", "Name");}
+    return $entries;
+}
+
+function fileinclude($Filename){//pass __FILE__
+    if ($_SERVER["SERVER_NAME"]){
+        return '<FONT COLOR="RED">Include: ' . $Filename . '</FONT>';
+    }
+}
+
+function edit_profiletype($ID = "", $Name, $Hierarchy, $Permissions = ""){
+    if(!$ID){
+        $ID = new_profiletype($Name);
+    }
+    logevent("Changed profile type: " . $ID . " (" . $Name . ", " . $Hierarchy . ", " . print_r($Permissions, true) . ")", false);
+    $data = array("Name" => $Name, "Hierarchy" => $Hierarchy);
+    if ($Permissions == "ALL"){
+        $Permissions = get_profile_permissions();
+    }
+    if (is_array($Permissions)) {
+        foreach ($Permissions as $Permission) {
+            $data[$Permission] = "1";
+        }
+    }
+    update_database("profiletypes", "ID", $ID, $data);
+    return $ID;
+}
+
+////////////////////////////////////Profile API/////////////////////////////////////////
+function read($Name){
+    return Session::get('Profile.' . $Name);
+}
+function salt(){
+    return "18eb00e8-f835-48cb-bbda-49ee6960261f";
+}
+
+function enum_profiles($Key, $Value){
+    return enum_all('profiles', $Key, $Value);
+}
+
+function get_profile($ID = ""){
+    if(!$ID){$ID=read("ID");}
+    return get_entry("profiles", $ID);
+}
+
+function is_email_in_use($EmailAddress, $NotByUserID=0){
+    $EmailAddress = clean_email($EmailAddress);
+    if($NotByUserID) {
+        return first("SELECT * FROM profiles WHERE Email = '" . $EmailAddress. "' AND ID != " . $NotByUserID);
+    } else {
+        return get_entry("profiles",$EmailAddress, "Email");
+    }
+}
+
+function get_profile_type($ProfileID, $GetByType = false){
+    if($GetByType){return get_entry("profiletypes", $ProfileID);}
+    $profiletype = get_entry("profiles", $ProfileID, "ID")->profileType;
+    return get_entry("profiletypes", $profiletype);
+}
+
+function can_profile_create($ProfileID, $ProfileType){
+    $creatorprofiletype = get_profile_type($ProfileID);
+    if($creatorprofiletype->CanCreateProfiles){
+        $ProfileType = get_profile_type($ProfileType, true);
+        return $creatorprofiletype->Hierarchy < $ProfileType->Hierarchy;
+    }
+}
+
+function randomPassword($Length=8) {
+    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+    $pass = "";
+    $alphaLength = strlen($alphabet) - 1;
+    for ($i = 0; $i < $Length; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass .= $alphabet[$n];
+    }
+    return $pass;
+}
+
+function is_valid_email($EmailAddress){
+    //http://php.net/manual/en/function.filter-var.php
+    //filter_var can also validate: FILTER_VALIDATE_IP FILTER_VALIDATE_INT FILTER_VALIDATE_BOOLEAN FILTER_VALIDATE_URL FILTER_SANITIZE_STRING
+    //flags FILTER_NULL_ON_FAILURE FILTER_FLAG_PATH_REQUIRED FILTER_FLAG_STRIP_LOW FILTER_FLAG_STRIP_HIGH
+    $EmailAddress = clean_email($EmailAddress);
+    if ($EmailAddress && filter_var($EmailAddress, FILTER_VALIDATE_EMAIL)) {
+        return $EmailAddress;
+    }
+}
+
+function find_profile($EmailAddress, $Password){
+    //echo salt();die();
+    $EmailAddress = clean_email($EmailAddress);
+    $Password = md5($Password . salt());
+    return enum_all("profiles", array("Email" => $EmailAddress, "Password" => $Password))->first();
+}
 
 
 
@@ -37,11 +149,22 @@ function tableexists($Table, $Column = ""){
     return \Schema::hasTable($Table);
 }
 
-function getColumnNames($Table, $Full = false){
+function getColumnNames($Table, $Ignore ="", $Full = false){
+    if(!is_array($Ignore)){$Ignore = array($Ignore);}
     if($Full){
-        return select_query("SHOW columns FROM " . $Table);
+        $Cols = select_query("SHOW columns FROM " . $Table);
+    } else {
+        $Cols = \Schema::getColumnListing($Table);
     }
-    return \Schema::getColumnListing($Table);
+    if(!count($Ignore)) {return $Cols;}
+    $Columns = array();
+    foreach ($Cols as $Key => $ColData) {
+        if($Full){$ColumnName = $ColData["Field"];} else {$ColumnName = $ColData;}
+        if (!in_array($ColumnName, $Ignore)) {
+            $Columns[] = $ColData;
+        }
+    }
+    return $Columns;
 }
 
 
@@ -67,6 +190,26 @@ function getColumnNames($Table, $Full = false){
 
 
 
+
+function clean_phone($Phone){
+    $Phone = kill_non_numeric($Phone, "+");//add a check to be sure only the first digit is a +
+    if($Phone != "+") {
+        return $Phone;
+    }
+}
+function clean_email($Email){
+    return strtolower(trim($Email));
+}
+function clean_postalcode($PostalCode){
+    $PostalCode = str_replace(" ", "", strtoupper(trim($PostalCode)));
+    if(validateCanadaZip($PostalCode)){
+        $delimeter = "";//" "
+        return left($PostalCode, 3) . $delimeter . right($PostalCode, 3);
+    }
+}
+function validateCanadaZip($PostalCode)  {//function by Roshan Bhattara(http://roshanbh.com.np)
+    return preg_match("/^([a-ceghj-npr-tv-z]){1}[0-9]{1}[a-ceghj-npr-tv-z]{1}[0-9]{1}[a-ceghj-npr-tv-z]{1}[0-9]{1}$/i", $PostalCode);
+}
 
 
 function debug_string_backtrace() {
@@ -81,19 +224,32 @@ function implode2($Array, $SmallGlue, $BigGlue){
     }
     return implode($Array,$BigGlue);
 }
-function debug($Iterator){
-    $Backtrace = debug_string_backtrace();
-    echo '<B>' . $Backtrace["file"] . ' (line ' . $Backtrace["line"] . ') From function: ' . $Backtrace["function"] . '(' . implode2($Backtrace["args"], "=", ",") . ');</B><BR>' ;
+function debug($Iterator, $DoStacktrace = true){
+    if($DoStacktrace) {
+        $Backtrace = debug_string_backtrace();
+        echo '<B>' . $Backtrace["file"] . ' (line ' . $Backtrace["line"] . ') From function: ' . $Backtrace["function"] . '();</B> ';
+    }
 
     if(is_array($Iterator)){
+        echo '(array)<BR>';
         var_dump($Iterator);
     } else if (is_object($Iterator)) {
-        foreach ($Iterator as $It) {
-            var_dump($It);
+        if(is_iterable($Iterator)) {
+            echo '(object array)<BR>';
+            foreach ($Iterator as $It) {
+               debug($It, false);
+            }
+        } else {
+            echo '(object)<BR>';
+            var_dump($Iterator);
         }
     } else {
+        echo '(value)<BR>';
         echo $Iterator;
     }
+}
+function is_iterable($var) {
+    return (is_array($var) || $var instanceof Traversable);
 }
 
 // My common functions
@@ -188,7 +344,7 @@ function enum_all($Table, $conditions = "1=1", $order = ""){
 function enum_anything($Table, $Key, $Value){
     return select_field_where($Table, array($Key => $Value), false);
 }
-function get_entry($Table, $Value, $PrimaryKey = ""){
+function get_entry($Table, $Value, $PrimaryKey = "ID"){
     if(!$PrimaryKey){$PrimaryKey = get_primary_key($Table);}
     return select_field_where($Table, array($PrimaryKey => $Value));
 }
@@ -199,7 +355,7 @@ function get_entry($Table, $Value, $PrimaryKey = ""){
 /////////////////////////RAW SQL
 function get_primary_key($Table){
     if (is_string($Table)){
-        $Table = getColumnNames($Table, true);
+        $Table = getColumnNames($Table, "", true);
     }
     if (is_object($Table)) {
         foreach ($Table as $Key => $Value) {
