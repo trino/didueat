656b42434b89b7d375e18f2cf90a3cf86a7fab30
biki <info@trinoweb.com>
2015-10-02 10:21:34 +0545

    error

--- app/Http/Functions/helpers.php
+++ app/Http/Functions/helpers.php
@@ -1297,7 +1297,8 @@ function handle_upload($Dir){
         $ext = end($arr);
         $file = date('YmdHis') . '.' . $ext;//unique filename
         move_uploaded_file($_FILES['myfile']['tmp_name'], APP . '../webroot/' . $Dir . $file);
-        $file_path = request->webroot . $Dir . $file;
+        // make it work
+        //$file_path = request->webroot . $Dir . $file;
             return $file_path;
         }
 }
