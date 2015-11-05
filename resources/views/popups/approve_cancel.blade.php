75e3e44aba8387de7e6a1278657d834572e02e36
trino <minesh@trinoweb.com>
2015-11-05 11:50:33 -0500

    Roy 11:50
    approve/cancel/disapprove popup:
    -shows the order ID # in the confirmation popup
    -doesn't have a confirmation popup if no text is entered, thus going
    straight to the missing data popup

--- resources/views/popups/approve_cancel.blade.php
+++ resources/views/popups/approve_cancel.blade.php
@@ -1,3 +1,5 @@
+<INPUT TYPE="hidden" id="orderid">
+
 <div id="approve-popup" style="display:none;width:500px;">
     <div class="login-pop-up">
         <div class="login-form" style="">
@@ -7,11 +9,11 @@
                 {!! Form::open(array('url' => '/restaurant/orders/list/approve', 'id'=>'approve-form','class'=>'form-horizontal form-without-legend','method'=>'post','role'=>'form')) !!}
                 <div class="form-group">
                     <label>Note: </label>
-                    <textarea name="note" rows="6" class="form-control" maxlength="5000" required></textarea>
+                    <textarea name="note" rows="6" id="approvetext" class="form-control" maxlength="5000" required></textarea>
                     <input type="hidden" name="id" value="" />
                 </div>
                 <div class="form-group">
-                    <input class="btn red" type="submit" Value=" Approve " onclick="return confirm(' Are you sure you want to approve this order ? ');">
+                    <input class="btn red" type="submit" Value=" Approve " onclick="return confirm2('approve');">
                 </div>
                 <div class="clearfix"></div>
                 {!! Form::close() !!}
@@ -29,11 +31,11 @@
                 {!! Form::open(array('url' => '/restaurant/orders/list/disapprove', 'id'=>'disapprove-form','class'=>'form-horizontal form-without-legend','method'=>'post','role'=>'form')) !!}
                 <div class="form-group">
                     <label>Note: </label>
-                    <textarea name="note" rows="6" class="form-control" maxlength="5000" required></textarea>
+                    <textarea name="note" rows="6" id="disapprove" class="form-control" maxlength="5000" required></textarea>
                     <input type="hidden" name="id" value="" />
                 </div>
                 <div class="form-group">
-                    <input class="btn red" type="submit" Value=" Disapprove " onclick="return confirm(' Are you sure you want to disapprove this order ? ');">
+                    <input class="btn red" type="submit" Value=" Disapprove " onclick="return confirm2('disapprove');">
                 </div>
                 <div class="clearfix"></div>
                 {!! Form::close() !!}
@@ -51,11 +53,11 @@
                 {!! Form::open(array('url' => '/restaurant/orders/list/cancel', 'id'=>'cancel-form','class'=>'form-horizontal form-without-legend','method'=>'post','role'=>'form')) !!}
                 <div class="form-group">
                     <label>Note: </label>
-                    <textarea name="note" rows="6" class="form-control" maxlength="5000" required></textarea>
+                    <textarea name="note" id="canceltext" rows="6" class="form-control" maxlength="5000" required></textarea>
                     <input type="hidden" name="id" value="" />
                 </div>
                 <div class="form-group">
-                    <input class="btn red" type="submit" Value=" Cancel " onclick="return confirm(' Are you sure you want to cancel this order ? ');">
+                    <input class="btn red" type="submit" Value=" Cancel " onclick="return confirm2('cancel');">
                 </div>
                 <div class="clearfix"></div>
                 {!! Form::close() !!}
@@ -63,25 +65,41 @@
         </div>
     </div>
 </div>
-
 <script>
+    function setid(id){
+       document.getElementById("orderid").value = id;
+    }
+    function getid(){
+        return document.getElementById("orderid").value;
+    }
+    function confirm2(Action){
+        var element = document.getElementById(Action + "text").value.length;
+        if(element==0){
+            return true;
+        }
+        return confirm('Are you sure you want to ' + Action + ' order # ' + getid() + '?');
+    }
+
     $(document).ready(function () {
         $('body').on('click', '.approve-popup', function(){
             var id = $(this).attr('data-id');
             $('#approve-form textarea[name=note]').val('');
             $('#approve-form input[name=id]').val(id);
+            setid(id);
         });
 
         $('body').on('click', '.disapprove-popup', function(){
             var id = $(this).attr('data-id');
             $('#disapprove-form textarea[name=note]').val('');
             $('#disapprove-form input[name=id]').val(id);
+            setid(id);
         });
 
         $('body').on('click', '.cancel-popup', function(){
             var id = $(this).attr('data-id');
             $('#cancel-form textarea[name=note]').val('');
             $('#cancel-form input[name=id]').val(id);
+            setid(id);
         });
     });
 </script>
