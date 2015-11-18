391bc66c80317f6304b43c2e18cc7e610babcdac
trino <minesh@trinoweb.com>
2015-11-18 09:52:57 -0500

    Roy 9:50

--- resources/views/layouts/includes/leftsidebar.blade.php
+++ resources/views/layouts/includes/leftsidebar.blade.php
@@ -49,7 +49,7 @@
                         <ul class="list-group margin-bottom-25 sidebar-menu">
                             <li class="list-group-item clearfix"><a href="{{ url('restaurant/orders/list') }}" class="<?php if (Request::path() == 'restaurant/orders/list') { echo 'active'; } ?>"><i class="fa fa-angle-right"></i> My Orders</a></li>
                             <li class="list-group-item clearfix"><a href="{{ url('restaurant/menus-manager') }}" class="<?php if (Request::path() == 'restaurant/menus-manager') { echo 'active'; } ?>"><i class="fa fa-angle-right"></i> Menu Manager</a> </li>
-                            <li class="list-group-item clearfix"><a href="{{ url('restaurant/addresses') }}" class="<?php if (Request::path() == 'restaurant/addresses') { echo 'active'; } ?>"><i class="fa fa-angle-right"></i> Notifications</a></li>
+                            <li class="list-group-item clearfix"><a href="{{ url('restaurant/addresses') }}" class="<?php if (Request::path() == 'restaurant/addresses') { echo 'active'; } ?>"><i class="fa fa-angle-right"></i> Notification Addresses</a></li>
                             <li class="list-group-item clearfix"><a href="{{ url('restaurant/info') }}" class="<?php if (Request::path() == 'restaurant/info') { echo 'active'; } ?>"><i class="fa fa-angle-right"></i> Restaurant Info</a></li>
                             <!--<li class="list-group-item clearfix"><a href="{{ url('restaurant/orders/pending') }}" class="<?php if (Request::path() == 'restaurant/orders/pending') {
                                 echo 'active';
