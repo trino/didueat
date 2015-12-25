ec06fd5d2857454f7c6cb2d2f53c13ad0f9a30b8
Van <info@trinoweb.com>
2015-12-25 15:52:50 -0500

    3
    3

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -21,11 +21,11 @@ if (Request::path() !== null && Request::path() != "/") {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="content-language" content="en-ca"/>
-    <meta http-equiv="content-language" content="en-ca"/>
 
     <meta content="{{ (isset($title))?$title.' | ':'' }}Did u eat" name="keywords">
     <meta content="Didueat" name="author">
+    <meta name="content-language" content="fr-CA"/>
+    <meta http-equiv="content-language" content="fr-CA"/>
     <meta content="{{ (isset($meta_description))? substr($meta_description,0,160):'didueat.com is very good from all over the world.' }}"
           name="description">
 
@@ -36,62 +36,63 @@ if (Request::path() !== null && Request::path() != "/") {
     <meta property="og:type" content="website">
     <meta property="og:image" content="-CUSTOMER VALUE-">
     <meta property="og:url" content="{{ url('/') . $nextPath }}">
-
     <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"/>
     <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon"/>
 
-    <!-- css -->
+
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css"
           integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
-    <link href="{{ asset('assets/global/css/custom_css.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/global/scripts/jqueryui/jquery-ui.css') }}" rel="stylesheet">
 
-    <!-- js -->
+        <link href="{{ asset('assets/global/css/custom_css.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/global/scripts/jqueryui/jquery-ui.css') }}" rel="stylesheet">
+
+
+
+    <script src="{{ asset('assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('assets/global/scripts/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/jquery-migrate.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}"
+            type="text/javascript"></script>
     <script src="{{ asset('assets/global/scripts/menu_manager.js') }}"></script>
     <script src="{{ asset('assets/global/scripts/upload.js') }}"></script>
     <script src="{{ asset('assets/global/scripts/jqueryui/jquery-ui.js') }}"></script>
-    <script src="{{ asset('assets/global/scripts/layout.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/global/scripts/jquery.tag-editor.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/global/scripts/jquery.cookie.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/global/plugins/jquery-migrate.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.min.js') }}"
+            type="text/javascript"></script>
     <script src="{{ asset('assets/global/plugins/fancybox/source/jquery.fancybox.pack.js') }}"
             type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.min.js') }}"
+            type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/zoom/jquery.zoom.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js') }}"
+            type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/slider-layer-slider/js/greensock.js') }}"
+            type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/slider-layer-slider/js/layerslider.transitions.js') }}"
+            type="text/javascript"></script>
+    <script src="{{ asset('assets/global/plugins/slider-layer-slider/js/layerslider.kreaturamedia.jquery.js') }}"
+            type="text/javascript"></script>
+    <script src="{{ asset('assets/global/scripts/layerslider-init.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/scripts/layout.js') }}" type="text/javascript"></script>
 
-    <!-- delete -->
-    @if(false)
-        <script src="{{ asset('assets/global/scripts/jquery.caret.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}"
-                type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.min.js') }}"
-                type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.min.js') }}"
-                type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/zoom/jquery.zoom.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js') }}"
-                type="text/javascript"></script>
-        <script src="{{ asset('assets/global/scripts/layerslider-init.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/slider-layer-slider/js/greensock.js') }}"
-                type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/slider-layer-slider/js/layerslider.transitions.js') }}"
-                type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/slider-layer-slider/js/layerslider.kreaturamedia.jquery.js') }}"
-                type="text/javascript"></script>
-    @endif
-    @if(false)
-        <link href="{{ asset('assets/global/css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-        <!--google font open sans -->
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic'
-              rel='stylesheet' type='text/css'>
-        <!--google font Montserrat -->
-        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500|Arimo&subset=latin,latin-ext'
-              rel='stylesheet' type='text/css'>
+    <script src="{{ asset('assets/global/scripts/jquery.tag-editor.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/scripts/jquery.caret.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/scripts/jquery.cookie.min.js') }}" type="text/javascript"></script>
+
+
+        @if(false)
+            <link href="{{ asset('assets/global/css/bootstrap.min.css') }}" rel="stylesheet">
+            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+            <!--google font open sans -->
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic'
+                  rel='stylesheet' type='text/css'>
+            <!--google font Montserrat -->
+            <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+            <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500|Arimo&subset=latin,latin-ext'
+                  rel='stylesheet' type='text/css'>
         @endif
-                <!-- /delete -->
+
 
 </head>
 
