338c2beaa9b88a6a0fbc1913d2b4fb9f9a0d0a3e
trino <minesh@trinoweb.com>
2015-12-28 10:32:15 -0500

    Roy 10:30
    one edit address

--- resources/views/common/editaddress.blade.php
+++ resources/views/common/editaddress.blade.php
@@ -1,6 +1,10 @@
 <?php
     printfile("views/common/editaddress.blade.php");
     $countries_list = \App\Http\Models\Countries::get();//load all countries
+
+    if(!isset($addresse_detail) && isset($resturant)){
+        $addresse_detail = $resturant;
+    }
 ?>
 
 <div class="form-group row">
@@ -13,21 +17,33 @@
 <div class="form-group row">
     <label class=" col-sm-3">Postal Code</label>
     <div class="col-sm-9">
-        <input type="text" name="post_code" class="form-control" placeholder="Postal Code" value="{{ (isset($addresse_detail->post_code))?$addresse_detail->post_code:'' }}">
+        <input type="text" name="post_code" class="form-control" placeholder="Postal Code" value="<?php
+            if (isset($addresse_detail->post_code)) {
+                echo $addresse_detail->post_code;
+            } else if (isset($addresse_detail->postal_code)) {
+                echo $addresse_detail->postal_code;
+            }
+        ?>">
     </div>
 </div>
 
 <div class="form-group row">
     <label class=" col-sm-3">Phone Number</label>
     <div class="col-sm-9">
-        <input type="text" name="phone_no" class="form-control" placeholder="Phone Number" value="{{ (isset($addresse_detail->phone_no))?$addresse_detail->phone_no:'' }}">
+        <input type="text" name="phone_no" class="form-control" placeholder="Phone Number" value="<?php
+            if (isset($addresse_detail->phone_no)){
+                echo $addresse_detail->phone_no;
+            } else if (isset($addresse_detail->phone)){
+                echo $addresse_detail->phone;
+            }
+        ?>">
     </div>
 </div>
 
 <div class="form-group row">
     <label class=" col-sm-3">Country </label>
     <div class="col-sm-9">
-        <select name="country" class="form-control" id="country2" required onchange="provinces('{{ addslashes(url("ajax")) }}', '{{ (isset($addresse_detail->province))?$addresse_detail->province:'' }}');">
+        <select name="country" class="form-control" id="country2" required onchange="provinces('{{ addslashes(url("ajax")) }}', '{{ (isset($addresse_detail->province))?$addresse_detail->province:'ON' }}');">
             <option value="">-Select One-</option>
             @foreach($countries_list as $value)
                 <option value="{{ $value->id }}" {{ (isset($addresse_detail->country) && $addresse_detail->country == $value->id)? 'selected' :'' }}>{{ $value->name }}</option>
@@ -48,6 +64,6 @@
 <div class="form-group row">
     <label class=" col-sm-3">City </label>
     <div class="col-sm-9">
-        <input type="text" name="city" class="form-control" id="city2" required>
+        <input type="text" name="city" class="form-control" id="city2" required value="{{ (isset($addresse_detail->city))?$addresse_detail->city:'' }}">
     </div>
 </div>
