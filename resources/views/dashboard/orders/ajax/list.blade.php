9b3fc35a800b45754cf2bc0616bd9a8adfbc5fb9
trino <minesh@trinoweb.com>
2016-01-05 13:22:54 -0500

    Roy 1:20
    response time

--- resources/views/dashboard/orders/ajax/list.blade.php
+++ resources/views/dashboard/orders/ajax/list.blade.php
@@ -1,4 +1,7 @@
-{{ printfile("views/dashboard/orders/ajax/list.blade.php") }}
+<?php
+    echo printfile("views/dashboard/orders/ajax/list.blade.php");
+    $secondsper = array("day" => 86400, "hour" => 3600, "minute" => 60);//"week" => 604800, 
+?>
 
 @if(\Session::has('message'))
     {!! message_show("Message!", \Session::get('message')) !!}
@@ -6,99 +9,102 @@
 
 <div class="card">
     <div class="card-header">
-
-
         <div class="row">
-            <div class="col-lg-6"><h6>My Orders ({{ ucwords($type) }}) <a class="btn btn-primary btn-sm "
-                                                                          href="{{ url('restaurant/report') }}"
-                                                                          class="">Print Report</a>
+            <div class="col-lg-6">
+                <h6>
+                    My Orders ({{ ucwords($type) }})
+                    <a class="btn btn-primary btn-sm " href="{{ url('restaurant/report') }}" class="">Print Report</a>
                 </h6>
             </div>
-
-
             @include('common.table_controls')
-
-
         </div>
-
-
     </div>
-
 
     <div class="card-block p-a-0">
         <table class="table table-responsive">
             <thead>
-            <tr>
-                <th>
-                    <a class="sortOrder" data-meta="id" data-order="ASC" data-title="ID" title="Sort ID ASC"><i
-                                class="fa fa-caret-down"></i></a>
-                    Order Id
-                    <a class="sortOrder" data-meta="id" data-order="DESC" data-title="ID" title="Sort ID DESC"><i
-                                class="fa fa-caret-up"></i></a>
-                </th>
-                <th>
-                    <a class="sortOrder" data-meta="ordered_by" data-order="ASC" data-title="Ordered By"
-                       title="Sort [Ordered By] ASC"><i class="fa fa-caret-down"></i></a>
-                    By
-                    <a class="sortOrder" data-meta="ordered_by" data-order="DESC" data-title="Ordered By"
-                       title="Sort [Ordered By] DESC"><i class="fa fa-caret-up"></i></a>
-                </th>
-                <th>
-                    <a class="sortOrder" data-meta="order_time" data-order="ASC" data-title="Date/Time"
-                       title="Sort [Date/Time] ASC"><i class="fa fa-caret-down"></i></a>
-                    On
-                    <a class="sortOrder" data-meta="order_time" data-order="DESC" data-title="Date/Time"
-                       title="Sort [Date/Time] DESC"><i class="fa fa-caret-up"></i></a>
-                </th>
-                <th>
-                    <a class="sortOrder" data-meta="status" data-order="ASC" data-title="Status"
-                       title="Sort [Status] ASC"><i class="fa fa-caret-down"></i></a>
-                    Status
-                    <a class="sortOrder" data-meta="status" data-order="DESC" data-title="Status"
-                       title="Sort [Status] DESC"><i class="fa fa-caret-up"></i></a>
-                </th>
-                <th width="36%">
-                    Actions
-                </th>
-            </tr>
+                <tr>
+                    <th>
+                        <a class="sortOrder" data-meta="id" data-order="ASC" data-title="ID" title="Sort ID ASC"><i class="fa fa-caret-down"></i></a>
+                        Order Id
+                        <a class="sortOrder" data-meta="id" data-order="DESC" data-title="ID" title="Sort ID DESC"><i class="fa fa-caret-up"></i></a>
+                    </th>
+                    <th>
+                        <a class="sortOrder" data-meta="ordered_by" data-order="ASC" data-title="Ordered By" title="Sort [Ordered By] ASC"><i class="fa fa-caret-down"></i></a>
+                        By
+                        <a class="sortOrder" data-meta="ordered_by" data-order="DESC" data-title="Ordered By" title="Sort [Ordered By] DESC"><i class="fa fa-caret-up"></i></a>
+                    </th>
+                    <th>
+                        <a class="sortOrder" data-meta="order_time" data-order="ASC" data-title="Date/Time" title="Sort [Date/Time] ASC"><i class="fa fa-caret-down"></i></a>
+                        On
+                        <a class="sortOrder" data-meta="order_time" data-order="DESC" data-title="Date/Time" title="Sort [Date/Time] DESC"><i class="fa fa-caret-up"></i></a>
+                    </th>
+                    <th>
+                        <a class="sortOrder" data-meta="status" data-order="ASC" data-title="Status" title="Sort [Status] ASC"><i class="fa fa-caret-down"></i></a>
+                        Status
+                        <a class="sortOrder" data-meta="status" data-order="DESC" data-title="Status" title="Sort [Status] DESC"><i class="fa fa-caret-up"></i></a>
+                    </th>
+                    <th>
+                        Actions
+                    </th>
+                    <TH>
+                        Response time
+                    </TH>
+                </tr>
             </thead>
             <tbody>
-            @if($recCount > 0)
-                @foreach($Query as $value)
-                    <tr>
-                        <td>{{ $value->id }}</td>
-                        <td>{{ $value->ordered_by }}</td>
-                        <td>{{ date('d M, Y H:i A', strtotime($value->order_time)) }}</td>
-                        <td>{{ $value->status }}</td>
-                        <td>
-                            @if(Session::get('session_profiletype') >= 1)
-                                <a href="{{ url('orders/order_detail/' . $value->id . '/' . $type) }}" class="btn btn-primary  btn-sm">View</a>
-                            @endif
+                @if($recCount > 0)
+                    @foreach($Query as $value)
+                        <tr>
+                            <td>{{ $value->id }}</td>
+                            <td>{{ $value->ordered_by }}</td>
+                            <td>{{ date('d M, Y H:i A', strtotime($value->order_time)) }}</td>
+                            <td>{{ $value->status }}</td>
+                            <td>
+                                @if(Session::get('session_profiletype') >= 1)
+                                    <a href="{{ url('orders/order_detail/' . $value->id . '/' . $type) }}" class="btn btn-primary  btn-sm">View</a>
+                                @endif
 
-                            @if(Session::get('session_profiletype') == 1)
-                                <a href="{{ url('orders/list/delete/'.$type.'/'.$value->id) }}"
-                                   class="btn btn-danger btn-sm"
-                                   onclick="return confirm('Are you sure you want to delete order # {{ $value->id }}?');">
-                                    Delete
-                                </a>
-                            @endif
-                        </td>
+                                @if(Session::get('session_profiletype') == 1)
+                                    <a href="{{ url('orders/list/delete/'.$type.'/'.$value->id) }}"
+                                       class="btn btn-danger btn-sm"
+                                       onclick="return confirm('Are you sure you want to delete order # {{ $value->id }}?');">
+                                        Delete
+                                    </a>
+                                @endif
+                            </td>
+                            <TD>
+                                <?php
+                                    if($value->time){
+                                        //$secondsper = array("week" => 604800, "day" => 86400, "hour" => 3600, "minute" => 60);
+                                        $delay = array('second' => strtotime($value->time) - strtotime($value->order_time), "total" => "");
+                                        $total = array();
+                                        foreach($secondsper as $timeperiod => $seconds){
+                                            $delay[$timeperiod] = floor($delay["second"] / $seconds);
+                                            $delay["second"] = $delay["second"] - ($seconds * $delay[$timeperiod]);
+                                            if($delay[$timeperiod]){
+                                                $total[] = $delay[$timeperiod] . " " . $timeperiod . iif($delay[$timeperiod] != 1, "s");
+                                            }
+                                        }
+                                        if($delay["second"]){
+                                            $total[] = $delay["second"] . " second" . iif($delay["second"] != 1, "s");
+                                        }
+                                        echo implode(", ", $total);
+                                    }
+                                ?>
+                            </TD>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <th scope="row" colspan="5" class="text-center">No records found!</th>
                     </tr>
-                @endforeach
-            @else
-                <tr>
-                    <th scope="row" colspan="5" class="text-center">No records found!</th>
-                </tr>
-            @endif
+                @endif
             </tbody>
         </table>
-
-
     </div>
-
-
     <div class="card-footer clearfix">
-        {!! $Pagination !!}    </div>
-
-
+        {!! $Pagination !!}
+    </div>
 </div>
+
