33dd0d070b72300dc0050c2e7a5bc427d94998d8
trino <minesh@trinoweb.com>
2016-01-26 11:44:52 -0500

    Roy 11:45
    fix address entry

--- resources/views/common/editaddress.blade.php
+++ resources/views/common/editaddress.blade.php
@@ -20,50 +20,39 @@ if (!isset($is_disabled)) {
 }
 $isUser = isset($apartment);
 $needsmobile = isset($mobile);
+$restSignUp=!isset($addresse_detail);//no idea what this needs to be
 ?>
 <input type="hidden" name="latitude" id="latitude" value=""/>
 <input type="hidden" name="longitude" id="longitude" value=""/>
 <input type="hidden" name="formatted_addressForDB" id="formatted_addressForDB" value=""/>
+    
 <?php echo newrow($new, "Search Address", "", false); ?>
-@if($is_disabled)
-    <input type="text" id="formatted_address" disabled name="formatted_address" class="form-control"
-           value="{{ (isset($addresse_detail->address))?$addresse_detail->address: old('address') }}">
-@else
-    <DIV CLASS="nowrap">
+    @if($is_disabled)
+        <input type="text" id="formatted_address" disabled name="formatted_address" class="form-control" value="{{ (isset($addresse_detail->address))?$addresse_detail->address: old('address') }}">
+    @else
+        <DIV CLASS="nowrap">
+            <input type="text" name="formatted_address" id="formatted_address" class="form-control formatted_address" placeholder="Address, City or Postal Code" value="<?php
+            if (old('formatted_address')) {
+                echo old('formatted_address');
+                //} else if(isset($addresse_detail->address) && isset($addresse_detail->city) && isset($addresse_detail->province) && isset($addresse_detail->country)) {
+            } else if (isset($addresse_detail->address)) {
+                $country = select_field("countries", "id", $addresse_detail->country, "name");
+                //echo $addresse_detail->address . ", " . $addresse_detail->city . ', ' . $addresse_detail->province . ', ' . $country;
+                echo $addresse_detail->address;
+            }
+            $width = 59;
+            ?>" autocomplete="off" style="width: -moz-calc(100% - {{$width}}px); width: -webkit-calc(100% - {{$width}}px); width: calc(100% - {{$width}}px);">
+            &nbsp;
+            <a class="btn btn-primary headerbutton" id="header-search-button" onclick="geolocate(formatted_address);" style="position:absolute;top:0px;">
+                <i class="fa fa fa-compass"></i>
+            </a>
+        </DIV>
 
-
-
-        <input type="text" name="formatted_address" id="formatted_address" class="form-control formatted_address"
-               placeholder="Address, City or Postal Code" value="<?php
-        if (old('formatted_address')) {
-            echo old('formatted_address');
-            //} else if(isset($addresse_detail->address) && isset($addresse_detail->city) && isset($addresse_detail->province) && isset($addresse_detail->country)) {
-        } else if (isset($addresse_detail->address)) {
-            $country = select_field("countries", "id", $addresse_detail->country, "name");
-            //echo $addresse_detail->address . ", " . $addresse_detail->city . ', ' . $addresse_detail->province . ', ' . $country;
-            echo $addresse_detail->address;
-        }
-        $width = 59;
-        ?>" autocomplete="off" style="width: -moz-calc(100% - {{$width}}px); width: -webkit-calc(100% - {{$width}}px); width: calc(100% - {{$width}}px);">
-
-
-        <a class="btn btn-primary headerbutton" oldstyle="display: none;" id="header-search-button"
-           onclick="geolocate(formatted_address);" style="">
-            <i class="fa fa fa-compass"></i>
-        </a>
-
-
-    </DIV>
-    <DIV>
-        <span class="alldays">(Start typing address then click from dropdown to populate all address fields)</span>
-    </DIV>
+        <DIV>
+            <span class="alldays">(Start typing the address then click from the dropdown to populate all address fields)</span>
+        </DIV>
     @endif
-
-
-
-
-    </div>
-    </div>
+</div></div>
 
 
     <!--  newrow($new, "Street Address", "", $required); ?>
@@ -71,13 +60,12 @@ $needsmobile = isset($mobile);
     </div>
 </div-->
 
-    <?php echo newrow($new, "City", "", $required, 4); ?>
+<?= newrow($new, "City", "", $required, 4); ?>
     <input type="text" id="city" name="city" class="form-control"
            {{ $is_disabled }} {{$required}} value="{{ (isset($addresse_detail->city))?$addresse_detail->city:old('city') }}" {{$required}}>
-    </div>
-    </div>
+</div></div>
 
-    <?php echo newrow($new, "Province", "", true, 4); ?>
+<?= newrow($new, "Province", "", true, 4); ?>
     <select name="province" id="province" class="form-control" {{ $is_disabled }} {{$required}}>
         <option value="">-Select One-</option>
         @foreach(select_field_where("states", "", false, "name", "ASC") as $value)
@@ -85,17 +73,15 @@ $needsmobile = isset($mobile);
         @endforeach
     </select>
     <!--input type="text" id="province" name="province" class="form-control" {{$required}} value="{{ (isset($addresse_detail->province))?$addresse_detail->province:old('province') }}"-->
-    </div>
-    </div>
+</div></div>
 
-    <?php echo newrow($new, "Postal Code", "", true, 4); ?>
+<?= newrow($new, "Postal Code", "", true, 4); ?>
     <input type="text" name="postal_code" id="postal_code" {{ $is_disabled }} class="form-control"
            placeholder="Postal Code"
            value="{{ (isset($addresse_detail->postal_code))?$addresse_detail->postal_code: old('postal_code') }}">
-    </div>
-    </div>
+</div></div>
 
-    <?php echo newrow($new, "Country", "", true, 4); ?>
+<?= newrow($new, "Country", "", true, 4); ?>
     <select name="country" id="country" class="form-control" {{ $is_disabled }} id="country2"
             {{$required}} onOLDchange="provinces('{{ addslashes(url("ajax")) }}', '{{ (isset($addresse_detail->province))?$addresse_detail->province: old('province') }}');">
 
@@ -108,56 +94,44 @@ $needsmobile = isset($mobile);
         @endforeach
     </select>
     <!--input type="text" id="country" name="country" class="form-control" {{$required}} value="{{ (isset($addresse_detail->country))?$addresse_detail->country:old('country') }}"-->
-    </div>
-    </div>
+</div></div>
 
-    <?php echo newrow($new, "Phone Number", "", $required, 4); ?>
+<?php echo newrow($new, "Phone Number", "", $required, 4); ?>
     <input type="text" name="phone" class="form-control"
            {{ $is_disabled }} placeholder="Phone Number must be a valid, in-service, Canadian number"
            value="{{ (isset($addresse_detail->phone))?$addresse_detail->phone: old('phone') }}" {{$required}}>
-    </div>
-    </div>
+</div></div>
 
 
-    <?php echo newrow($new, "Cellphone Number", "", $required, 4); ?>
+<?php echo newrow($new, "Cellphone Number", "", $required, 4); ?>
     <input type="text" name="mobile" class="form-control" {{ $is_disabled }} placeholder="Cellphone Number"
            value="{{ (isset($addresse_detail->mobile))?$addresse_detail->mobile: old('mobile') }}" {{$required}}>
-    </div>
-    </div>
+</div></div>
 
 
-    <?php  
+<?php
     if(!$restSignUp){
 		    echo newrow($new,"Save","","",12,true); 
-    ?>
-
-		    <hr width="100%" align="center" />
-		    <button type="submit" class="btn btn-primary" style="margin-left:auto;margin-right:auto;">Save</button>
-		    </div>
-		    </div>
-		    
-    <?php 
+            echo '<hr width="100%" align="center" /><button type="submit" class="btn btn-primary" style="margin-left:auto;margin-right:auto;">Save</button></div></div>';
     }
     
-    if($isUser){ ?>
-    <?php echo newrow($new, "Apartment/Unit", "", false, 4); ?>
-    <input type="text" name="apartment" class="form-control" {{ $is_disabled }} placeholder="Apartment/Unit/Townhouse"
+    if($isUser){
+        echo newrow($new, "Apartment/Unit", "", false, 4); ?>
+            <input type="text" name="apartment" class="form-control" {{ $is_disabled }} placeholder="Apartment/Unit/Townhouse"
            value="{{ (isset($addresse_detail->apartment))?$addresse_detail->apartment:old('apartment') }}">
-    </div>
-    </div>
+        </div></div>
 
-    <?php echo newrow($new, "Buzz Code", "", false, 4); ?>
-    <input type="text" name="buzz" class="form-control" {{ $is_disabled }} placeholder="Buzz/ringer/doorbell Code"
-           value="{{ (isset($addresse_detail->buzz))?$addresse_detail->buzz:old('buzz') }}">
-    </div>
-    </div>
+        <?= newrow($new, "Buzz Code", "", false, 4); ?>
+        <input type="text" name="buzz" class="form-control" {{ $is_disabled }} placeholder="Buzz/ringer/doorbell Code"
+               value="{{ (isset($addresse_detail->buzz))?$addresse_detail->buzz:old('buzz') }}">
+        </div></div>
 
-    <?php echo newrow($new, "Notes", "", false, 9); ?>
-    <input type="text" name="notes" class="form-control" {{ $is_disabled }} placeholder="ie: Side door"
-           value="{{ (isset($addresse_detail->notes))?$addresse_detail->notes:old('notes') }}">
-    </div>
-    </div>
+        <?= newrow($new, "Notes", "", false, 9); ?>
+        <input type="text" name="notes" class="form-control" {{ $is_disabled }} placeholder="ie: Side door"
+               value="{{ (isset($addresse_detail->notes))?$addresse_detail->notes:old('notes') }}">
+        </div></div>
     <?php }
+
     if (isset($disclaimer)) {
         echo 'The entrance must be safe (ie: well-lit, clear of ice';
         if (date("md") == "0401") {
@@ -166,22 +140,22 @@ $needsmobile = isset($mobile);
         echo ') Drivers are not required to go up stairs, any time guarantees a store may have ends at the lobby, or when they call on arrival (whether or not they are able to reach you)';
     }
     if(isset($dontinclude)) { ?>
-    <SCRIPT>
-        $(document).ready(function () {
-            if (typeof(initAutocomplete) == "function") {
-                initAutocomplete();//editaddress
-            }
-        });
-    </SCRIPT>
+        <SCRIPT>
+            $(document).ready(function () {
+                if (typeof(initAutocomplete) == "function") {
+                    initAutocomplete();//editaddress
+                }
+            });
+        </SCRIPT>
     <?php } else {
-    echo '<script src="' . url("assets/global/scripts/provinces.js") . '" type="text/javascript"></script>';
-    if (!includeJS("https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=places&callback=initAutocomplete", "async defer")) {
-        //echo '<script>initAutocomplete();</script>';
-    }
-    ?>
-    <SCRIPT>
-        $(document).ready(function () {
-            cities("{{ url('ajax') }}", '{{ (isset($addresse_detail->city))?$addresse_detail->city:0 }}');
-        });
-    </SCRIPT>
+        echo '<script src="' . url("assets/global/scripts/provinces.js") . '" type="text/javascript"></script>';
+        if (!includeJS("https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=places&callback=initAutocomplete", "async defer")) {
+            //echo '<script>initAutocomplete();</script>';
+        }
+        ?>
+        <SCRIPT>
+            $(document).ready(function () {
+                cities("{{ url('ajax') }}", '{{ (isset($addresse_detail->city))?$addresse_detail->city:0 }}');
+            });
+        </SCRIPT>
     <?php } ?>
