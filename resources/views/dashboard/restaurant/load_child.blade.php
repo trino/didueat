9cfae76a4f115bab5726b801338b09f71169a3c2
Rob <rob@trinoweb.com>
2016-02-09 17:42:54 +0545

    changes

--- resources/views/dashboard/restaurant/load_child.blade.php
+++ resources/views/dashboard/restaurant/load_child.blade.php
@@ -12,30 +12,25 @@
             $i++;
             ?>
             <div class="cmore ignore ignore1" id="cmore{{ $cc->id }}">
-                @if($i != 1)
-                    <p class="addon_ignore ignore ignore2 ignore1">&nbsp;</p>
-                @endif
 
-
-
-                    <div class="col-md-8 col-sm-10 col-xs-10 ignore ignore2 ignore1">
+                    <div class="">
                         <div class="col-md-6 ">
                             <input class="form-control cctitle ignore ignore2 ignore1" type="text" placeholder="Item" value="{{ $cc->menu_item }}" />
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-2">
                             <input class="form-control ccprice ignore ignore2 ignore1 pricechk" type="text" placeholder="Price" value="{{ $cc->price }}" />
                         </div>
                     </div>
                     <div class="col-md-2 col-sm-2 col-xs-2 ignore top-padd ignore2">
-                        <a href="javascript:void(0);" class="btn ignore btn-danger btn-sm ignore2" onclick="$(this).parent().parent().remove();">
+                        <a href="javascript:void(0);" class="btn btn-secondary-outline btn-sm" onclick="$(this).parent().parent().remove();">
                         <span class="fa fa-close ignore ignore2 ignore1"></span>
                     </a>
                     </div>
                     <div class="resturant-arrows col-md-2 col-sm-2 col-xs-12">
-                        <a id="child_up_{{ $cc->id }}"  class="btn btn-sm btn-secondary sorting_child" href="javascript:void(0)">
+                        <a id="child_up_{{ $cc->id }}"  class="btn btn-xs btn-secondary sorting_child" href="javascript:void(0)">
                             <i class="fa fa-angle-up"></i>
                         </a>
-                        <a id="child_down_{{ $cc->id }}" class="btn btn-sm btn-secondary sorting_child" href="javascript:void(0)">
+                        <a id="child_down_{{ $cc->id }}" class="btn btn-xs btn-secondary sorting_child" href="javascript:void(0)">
                             <i class="fa fa-angle-down"></i>
                         </a>
                     </div>
