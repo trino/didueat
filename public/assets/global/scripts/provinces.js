36ee823da3de8ae69ecb21f18acee3a1bfa2bfdc
trino <minesh@trinoweb.com>
2016-02-23 12:14:19 -0500

    Roy 12:10

--- public/assets/global/scripts/provinces.js
+++ public/assets/global/scripts/provinces.js
@@ -355,22 +355,18 @@ function toRad(Value) {
     return Value * Math.PI / 180;
 }
 
-var unescape = (function() {
+function unescape (str) {
     // this prevents any overhead from creating the object each time
     var element = document.createElement('div');
 
-    function decodeHTMLEntities (str) {
-        if(str && typeof str === 'string') {
-            // strip script/html tags
-            str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
-            str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
-            element.innerHTML = str;
-            str = element.textContent;
-            element.textContent = '';
-        }
-
-        return str;
+    if (str && typeof str === 'string') {
+        // strip script/html tags
+        str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+        str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+        element.innerHTML = str;
+        str = element.textContent;
+        element.textContent = '';
     }
 
-    return decodeHTMLEntities;
-})();
+    return str;
+}
