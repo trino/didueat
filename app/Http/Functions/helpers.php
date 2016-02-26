1e67d3cccd98d9b0235132f4e4d5febcf127349f
Van <info@trinoweb.com>
2016-02-26 13:47:26 -0500

    test

--- app/Http/Functions/helpers.php
+++ app/Http/Functions/helpers.php
@@ -1922,7 +1922,7 @@ function datename($date){
             $Message = right($Message, strlen($Message)-8);
             switch($Message){
                 case "nostores": $Message = '<span id="countRows">No</span> restaurant<span id="countRowsS">s</span> found in your area'; break;
-                case "menuadd": $Message = "Item has been added/updated successfully"; break;
+                case "menuadd": $Message = "Menu item saved successfully"; break;
                 case "sorted": $Message = "Menu item moved successfully"; break;
 
                 case "user_fire":case "user_hire": case "user_possess": case "user_depossess": $Message = "User has been " . str_replace("eed", "ed", str_replace("user_", "", $Message) . "ed"); break;
