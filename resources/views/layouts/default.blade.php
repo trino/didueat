e82dc070a25c8061c993e12263400aa1eb1a5133
trino <minesh@trinoweb.com>
2016-02-29 13:35:45 -0500

    Roy 1:35

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -103,6 +103,18 @@ if (Request::path() !== null && Request::path() != "/") {
         </style>
     @endif
 
+<!--[if IE]>
+    <style>
+        .form-control{
+            padding-bottom: 10px !important;
+        }
+    </style>
+<![endif]-->
+<STYLE>
+    _:-ms-fullscreen, :root input.form-control{
+        padding-bottom: 10px !important;
+    }
+</STYLE>
 
 <SCRIPT>
     var baseurl = "{{ url('/') }}";
