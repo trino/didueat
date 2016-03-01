70aa9bc0d3543c3c3cfac3e2a9a3cb5b1d48c27e
trino <minesh@trinoweb.com>
2016-03-01 14:01:17 -0500

    Roy 2:00

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
-class VerifyCsrfToken extends BaseVerifier
-{
+class VerifyCsrfToken extends BaseVerifier {
     /**
      * The URIs that should be excluded from CSRF verification.
      *
@@ -14,4 +14,11 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next) {
+        if (!$request->secure() && islive()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
 }
