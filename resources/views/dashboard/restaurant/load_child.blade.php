88299a2bcf83278cbb66aa7a6b8353251aa61489
Rob <rob@trinoweb.com>
2016-03-03 23:27:05 +0545

    changes

--- resources/views/dashboard/restaurant/load_child.blade.php
+++ resources/views/dashboard/restaurant/load_child.blade.php
@@ -17,23 +17,22 @@
                         <div class="col-md-6 ">
                             <input class="form-control cctitle ignore ignore2 ignore1" type="text" placeholder="Item" value="{{ $cc->menu_item }}" />
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-3">
                             <input class="form-control ccprice ignore ignore2 ignore1 pricechk" type="text" placeholder="Price" value="{{ $cc->price }}" />
                         </div>
                     </div>
-                    <div class="col-md-2 col-sm-2 col-xs-2 ignore top-padd ignore2">
-                        <a href="javascript:void(0);" class="btn btn-secondary-outline btn-sm" onclick="$(this).parent().parent().remove();">
-                        <span class="fa fa-close ignore ignore2 ignore1"></span>
-                    </a>
-                    </div>
-                    <div class="resturant-arrows col-md-2 col-sm-2 col-xs-12">
-                        <a id="child_up_{{ $cc->id }}"  class="btn btn-sm btn-secondary sorting_child" href="javascript:void(0)">
-                            <i class="fa fa-arrow-up"></i>
-                        </a>
-                        <a id="child_down_{{ $cc->id }}" class="btn btn-sm btn-secondary sorting_child" href="javascript:void(0)">
-                            <i class="fa fa-arrow-down"></i>
-                        </a>
-                    </div>
+                    <div class="col-md-3">
+                                <div class="btn-group pull-right" role="group" aria-label="Basic example">
+                                    <button href="javascript:void(0)" id="child_up_{{ $cc->id }}"
+                                       class="btn btn-sm btn-secondary sorting_child"><i class="fa fa-arrow-up"></i></button>
+                                    <button href="javascript:void(0)" id="child_down_{{ $cc->id }}"
+                                       class="btn btn-sm btn-secondary sorting_child"><i
+                                                class="fa fa-arrow-down"></i></button>
+                                <button href="javascript:void(0);" class="btn btn-sm btn-secondary"
+                                   onclick="$(this).closest('.cmore').remove();" >
+                                    <i class="fa fa-times"></i> </button>
+                                </div>
+                            </div>
                     <div class="clearfix ignore ignore2 ignore1"></div>
                 
             </div>
