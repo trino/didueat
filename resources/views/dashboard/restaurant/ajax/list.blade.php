2998b7fe75b5f28cfc1cd4dd15006ce770564c31
trino <minesh@trinoweb.com>
2016-03-15 09:50:00 -0400

    Roy 9:45

--- resources/views/dashboard/restaurant/ajax/list.blade.php
+++ resources/views/dashboard/restaurant/ajax/list.blade.php
@@ -10,18 +10,6 @@
             <div class="col-lg-9">
                 <h4 class="card-title">
                     Restaurants
-
-                    <?php
-                        $incomplete = isset($_GET["incomplete"]);
-                        echo '<a class="btn btn-primary btn-sm" ';
-                        //href="{{ url('restaurant/list') }}">Add
-                        if($incomplete){
-                            echo 'HREF="' . url('/restaurant/list') . '">All';
-                        } else {
-                            echo 'HREF="' . url('/restaurant/list?incomplete') . '">Incomplete';
-                        }
-                        echo '</a>';
-                    ?>
                 </h4>
             </div>
             @include('common.table_controls')
@@ -31,11 +19,7 @@
     <div class="card-block p-a-0">
         <table class="table table-responsive m-b-0">
             <?php
-                if($incomplete){
-                    TH(array("id" => "ID", "name", "reasons" => array("sort" => false), "" => array("sort" => false)));
-                } else {
-                    TH(array("id" => "ID", 'logo' => array("sort" => false), "name", "rating", "status", "" => array("sort" => false)));
-                }
+                TH(array("id" => "ID", 'logo' => array("sort" => false), "name", "rating", "status", "" => array("sort" => false)));
             ?>
             <tbody>
                 @if($recCount > 0)
@@ -43,13 +27,12 @@
                 <?php $resLogo = (isset($value->logo) && $value->logo != "") ? 'restaurants/' . $value->id . '/thumb_' . $value->logo : 'default.png'; ?>
                 <tr>
                     <td>{{ $value->id }}</td>
-                    @if(!$incomplete)
-                        <td><img src="{{ asset('assets/images/'.$resLogo) }}" width="90"/></td>
-                    @endif
+                    <td><img src="{{ asset('assets/images/'.$resLogo) }}" width="90"/></td>
                     <td>{{ $value->name }}</td>
-
-                    @if($incomplete)
-                        <TD>
+                    <td NOWRAP>{!! rating_initialize("static-rating", "restaurant", $value['id'], true, 'update-rating', false) !!}</td>
+                    <td>
+                        @if(!$value->is_complete)
+                            <a class="btn btn-secondary-outline btn-sm" style="cursor: default;">Incomplete</A><HR CLASS="slimhr">
                             <?php
                                 $reasons = array();
                                 if(!$value->has_creditcard){
@@ -97,29 +80,21 @@
                                     }
                                 }
                             ?>
-                        </TD>
-                    @else
-                        <td NOWRAP>{!! rating_initialize("static-rating", "restaurant", $value['id'], true, 'update-rating', false) !!}</td>
-                        <td>
-                            @if(!$value->is_complete)
-                                <a class="btn btn-secondary-outline btn-sm" style="cursor: default;">Incomplete</A>
-                            @elseif($value->open == true)
-                                <a class="btn btn-secondary-outline btn-sm" style="cursor: default;">Enabled</A>
-                                    <a href="{{ url('restaurant/list/status/'.$value->id) }}" class="btn btn-warning btn-sm"
-                                   onclick="return confirm('Are you sure you want to disable {{ addslashes("'" . $value->name . "'") }} ?');">Disable</a>
-                            @else
-                                <a href="{{ url('restaurant/list/status/'.$value->id) }}" class="btn  btn-success btn-sm"
-                                   onclick="return confirm('Are you sure you want to enable {{ addslashes("'" . $value->name . "'") }} ?');">Enable</a>
-                                <a class="btn btn-secondary-outline btn-sm" style="cursor: default;">Disabled</A>
-                            @endif
-                        </td>
-                    @endif
+                        @elseif($value->open == true)
+                            <a class="btn btn-secondary-outline btn-sm" style="cursor: default;">Enabled</A>
+                                <a href="{{ url('restaurant/list/status/'.$value->id) }}" class="btn btn-warning btn-sm"
+                               onclick="return confirm('Are you sure you want to disable {{ addslashes("'" . $value->name . "'") }} ?');">Disable</a>
+                        @else
+                            <a href="{{ url('restaurant/list/status/'.$value->id) }}" class="btn  btn-success btn-sm"
+                               onclick="return confirm('Are you sure you want to enable {{ addslashes("'" . $value->name . "'") }} ?');">Enable</a>
+                            <a class="btn btn-secondary-outline btn-sm" style="cursor: default;">Disabled</A>
+                        @endif
+                    </td>
+
 
                     <td>
-                        @if(!$incomplete)
-                            <a href="{{ url('orders/list/restaurant/' . $value['id']) }}" class="btn btn-info btn-sm">Orders</a>
-                            <a href="{{ url('restaurants/' . $value->slug . '/menu/') }}" class="btn btn-info btn-sm">Menu</a>
-                        @endif
+                        <a href="{{ url('orders/list/restaurant/' . $value['id']) }}" class="btn btn-info btn-sm">Orders</a>
+                        <a href="{{ url('restaurants/' . $value->slug . '/menu/') }}" class="btn btn-info btn-sm">Menu</a>
                         <a href="{{ url('restaurant/info/'.$value->id) }}" class="btn btn-info btn-sm">Edit</a>
                         <a href="{{ url('restaurant/list/delete/'.$value->id) }}" class="btn btn-secondary-outline btn-sm" onclick="return confirm('Are you sure you want to delete {{ addslashes("'" . $value->name . "'") }} ?');">X</a>
                     </td>
@@ -136,9 +111,6 @@
     </div>
 
     @if(Session::get('session_type_user') == "super"  && $recCount > 10)
-
-
-    <div class="card-footer clearfix">
-        {!! $Pagination !!}    </div>
-        @endif
+        <div class="card-footer clearfix">{!! $Pagination !!}    </div>
+    @endif
 </div>
