57e60c35b558066f670bd8d29f7f6430bc937407
trino <minesh@trinoweb.com>
2016-03-17 14:44:40 -0400

    Roy 2:40

--- resources/views/dashboard/notifications_address/ajax/list.blade.php
+++ resources/views/dashboard/notifications_address/ajax/list.blade.php
@@ -21,80 +21,79 @@
 
     <div class="card-block p-a-0">
         <table class="table table-responsive m-b-0">
-            @if ($recCount > 0)
-                <thead>
-                    <tr>
-                        <th>Enable</th>
-                        <th>Notify</th>
-                        <th>Note</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($Query as $key => $value)
-                        <tr class="rows" data-id="{{ $value->id }}" data-order="{{ $key }}">
-                            <!--td style="min-width: 100px;">{{ $key+1 }}
-                                @if($recCount > 1)
-                                    &nbsp;
-                                    <div class="btn-group-vertical">
-                                        <a class="btn btn-secondary-outline up btn-sm"><i class="fa fa-arrow-up"></i></a>
-                                        <a class="btn btn-secondary-outline down btn-sm"><i
-                                                    class="fa fa-arrow-down"></i></a>
-                                    </div>
-                                @endif
-                            </td-->
-
-                            <td class="text-xs-center">
-                               <?php
-                                   $candisable = true;
-                                   foreach(array("email", "phone", "mobile") as $field){
-                                       if($value->address == $restaurant->$field){
-                                           $candisable = false;
-                                           break;
-                                       }
-                                   }
-
-                                   echo '<label class="c-input c-checkbox pull-left"><INPUT TYPE="CHECKBOX" ID="add_enable_' . $value->id . '" CLASS="fullcheck"';
-                                   if($candisable){
-                                       echo ' ONCLICK="add_enable(' . $value->id . ');"';
-                                       if ($value->enabled) {
-                                           echo ' CHECKED';
-                                       }
-                                   } else {
-                                       echo " CHECKED DISABLED";
-                                       $value->note = "This is required";
-                                   }
-                                   echo '><span class="c-indicator"></span></label>';
-
-                                   if($value->type != "Email"){
-                                       $value->address = phonenumber($value->address, true);
-                                   }
-                               ?>
-                            </td>
-
-                            <td>{{ $value->type }}: {{ $value->address }}</td>
-                            <td id="note_{{ $value->id }}" value="{{ $value->note }}" onclick="editnote({{ $value->id }});">{{ $value->note }}</td>
-
-                            <td>
-                                <div class=" pull-right ">
-                                    @if (Session::get('session_type_user') == "super")
-                                        <a class="btn btn-secondary-outline btn-sm editRow " data-toggle="modal" data-target="#editModel" data-id="{{ $value->id }}">Edit</a>
-                                    @endif
-                                    @if($candisable)
-                                        <a href="{{ url('notification/addresses/delete/'.$value->id) }}" class="btn btn-secondary-outline btn-sm"
-                                            onclick="return confirm('Are you sure you want to delete {{ addslashes($value->address) }}?');"><i class="fa fa-times"></i></a>
-                                    @endif
+            <thead>
+                <tr>
+                    <th>Enable</th>
+                    <th>Notify</th>
+                    <th>Note</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    if ($recCount == 0){
+                        echo '<tr><td colspan="4"><span class="text-muted">No Notifcations Methods Yet. Defaulting to:</span></td></tr>';
+                        $Query = array(array_to_object(array("id" => 0, "type" => "Email", "address" => $restaurant->email )));
+                    }
+                ?>
+                @foreach($Query as $key => $value)
+                    <tr class="rows" data-id="{{ $value->id }}" data-order="{{ $key }}">
+                        <!--td style="min-width: 100px;">{{ $key+1 }}
+                            @if($recCount > 1)
+                                &nbsp;
+                                <div class="btn-group-vertical">
+                                    <a class="btn btn-secondary-outline up btn-sm"><i class="fa fa-arrow-up"></i></a>
+                                    <a class="btn btn-secondary-outline down btn-sm"><i
+                                                class="fa fa-arrow-down"></i></a>
                                 </div>
-                            </td>
+                            @endif
+                        </td-->
 
-                        </tr>
-                    @endforeach
-            @else
-                <tbody>
-                    <tr>
-                        <td><span class="text-muted">No Notifcations Methods Yet</span></td>
+                        <td class="text-xs-center">
+                           <?php
+                               $candisable = true;
+                               foreach(array("email", "phone", "mobile") as $field){
+                                   if($value->address == $restaurant->$field){
+                                       $candisable = false;
+                                       break;
+                                   }
+                               }
+
+                               echo '<label class="c-input c-checkbox pull-left"><INPUT TYPE="CHECKBOX" ID="add_enable_' . $value->id . '" CLASS="fullcheck"';
+                               if($candisable){
+                                   echo ' ONCLICK="add_enable(' . $value->id . ');"';
+                                   if ($value->enabled) {
+                                       echo ' CHECKED';
+                                   }
+                               } else {
+                                   echo " CHECKED DISABLED";
+                                   $value->note = "This is required";
+                               }
+                               echo '><span class="c-indicator"></span></label>';
+
+                               if($value->type != "Email"){
+                                   $value->address = phonenumber($value->address, true);
+                               }
+                           ?>
+                        </td>
+
+                        <td>{{ $value->type }}: {{ $value->address }}</td>
+                        <td id="note_{{ $value->id }}" value="{{ $value->note }}" onclick="editnote({{ $value->id }});">{{ $value->note }}</td>
+
+                        <td>
+                            <div class=" pull-right ">
+                                @if (Session::get('session_type_user') == "super" && $value->id)
+                                    <a class="btn btn-secondary-outline btn-sm editRow " data-toggle="modal" data-target="#editModel" data-id="{{ $value->id }}">Edit</a>
+                                @endif
+                                @if($candisable)
+                                    <a href="{{ url('notification/addresses/delete/'.$value->id) }}" class="btn btn-secondary-outline btn-sm"
+                                        onclick="return confirm('Are you sure you want to delete {{ addslashes($value->address) }}?');"><i class="fa fa-times"></i></a>
+                                @endif
+                            </div>
+                        </td>
+
                     </tr>
-            @endif
+                @endforeach
             </tbody>
         </table>
     </div>
