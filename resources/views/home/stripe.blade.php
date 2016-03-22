90d1932acc348d7e7de15439aacfe12271a2ed5e
trino <minesh@trinoweb.com>
2016-03-22 10:17:10 -0400

    Roy 10:15

--- resources/views/home/stripe.blade.php
+++ resources/views/home/stripe.blade.php
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<!--script src="{{ asset('assets/global/scripts/stripe.js') }}"></script-->
+<script src="{{ asset('assets/global/scripts/form-validation.js') }}"></script>
 <?php
 	printfile("views/home/stripe.blade.php");
 	$orderDesc= DIDUEAT . " order";
@@ -8,15 +11,10 @@
 	if($CanSaveCard){
 		$CreditCards = select_field_where("credit_cards", array("user_id" => read("id"), "user_type" => "user"), false);
 	}
-?>
-<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-<!--script src="{{ asset('assets/global/scripts/stripe.js') }}"></script-->
-<script src="{{ asset('assets/global/scripts/form-validation.js') }}"></script>
 
-<?php if(!isset($loaded_from)){?>
-{!! Form::open(array('id'=>'payment-form','class'=>'form-horizontal','method'=>'post','role'=>'form')) !!}
-<br/>
-<?php }?>
+	if(!isset($loaded_from)){ ?>
+		{!! Form::open(array('id'=>'payment-form','class'=>'form-horizontal','method'=>'post','role'=>'form')) !!}<br/>
+<?php } ?>
 
 <div class="col-xs-12 p-t-1">
 	<span class="payment-errors instruct"></span>
@@ -28,7 +26,8 @@
 	<input name="orderID" type="hidden" class="S_orderID" value='{{ (isset($orderID))?$orderID:"" }}' />
 	<input type="hidden" name="stripeToken" value="" class="stripeToken"/>
 
-	<? if($CreditCards){ ?>		<div class="form-group row editcard">
+	<?php if($CreditCards){ ?>
+		<div class="form-group row editcard">
 			<label class="col-xs-5 text-xs-right">
 				Saved Card
 			</label>
@@ -36,13 +35,13 @@
 				<div class="input-icon">
 					<SELECT name="cardid" ID="cardid" class="form-control" onchange="changecard();">
 						<OPTION VALUE="">New Card</OPTION>
-						<?
+						<?php
 							foreach($CreditCards as $CreditCard){
 								$CardNumber = obfuscate(\Crypt::decrypt($CreditCard->card_number));
 								$Month = \Crypt::decrypt($CreditCard->expiry_month);
 								$Year = \Crypt::decrypt($CreditCard->expiry_year);
-								if($Year > date("y") || ($Year == date("y") && $Month >= date("n")) ){//check if it's expired
-									echo '<OPTION VALUE="' . $CreditCard->id . '">' . $CardNumber . '</OPTION>';//card number never reaches the user
+								if($Year > date("y") || ($Year == date("y") && $Month >= date("n")) ){
+									echo '<OPTION VALUE="' . $CreditCard->id . '">' . $CardNumber . '</OPTION>';
 								}
 							}
 						?>
@@ -50,52 +49,42 @@
 				</div>
 			</div>
 		</div>
-	<?}?>
+	<? } ?>
 
 	<div class="form-group row editcard">
 		<!--label aria-required="true" class="col-xs-3 text-xs-right required" id="card_number">Card #@if(debugmode())<i class="fa fa-credit-card" onclick="$('#cardnumber').val('4242424242424242');" TITLE="Click to use DEBUG MODE card"></i>@endif</label-->
+
 		<div class="col-xs-12">
-			<div class="input-icon">
-				<input aria-required="true" autocomplete="off" name="cardnumber" placeholder="Card Number" id="cardnumber" class="form-control" type="text" size="20" data-stripe="number" required/>
+			<div class="input-group">
+				<span class="input-group-addon" id="credit-card-addon"><i class="fa fa-fw fa-credit-card" onclick="$('#cardnumber').val('4242424242424242');" TITLE="Click to use DEBUG MODE card"></i></span>
+				<input aria-required="true" autocomplete="off" name="cardnumber" placeholder="Card Number" id="cardnumber" class="form-control" type="text" size="20" data-stripe="number" required aria-describedby="credit-card-addon" style="width:75%;"/>
+				<input aria-required="true" autocomplete="off" placeholder="CVC" name="cardcvc" class="form-control" type="text" size="4" data-stripe="cvc" required aria-describedby="cvc-addon" style="width:25%;"/>
 			</div>
 		</div>
 	</div>
 
 	<div class="form-group row editcard">
 		<!--label aria-required="true" class="col-xs-3 text-xs-right required" id="cvc">CVC</label-->
-		<div class="col-xs-3">
-			<div class="input-icon">
-				<input aria-required="true" autocomplete="off" placeholder="CVC" name="cardcvc" class="form-control" type="text" size="4" data-stripe="cvc" required/>
-			</div>
-		</div>
-
 		<!--label aria-required="true" class="col-xs-3 text-xs-right required" id="expiry">Expiry</label-->
-		<div class="col-xs-5">
-			<div class="input-icon">
-				<SELECT aria-required="true" name="cardmonth" class="form-control lesspadding" data-stripe="exp-month">
-					<OPTION value="01">January</OPTION>
-					<OPTION value="02">February</OPTION>
-					<OPTION value="03">March</OPTION>
-					<OPTION value="04">April</OPTION>
-					<OPTION value="05">May</OPTION>
-					<OPTION value="06">June</OPTION>
-					<OPTION value="07">July</OPTION>
-					<OPTION value="08">August</OPTION>
-					<OPTION value="09">September</OPTION>
-					<OPTION value="10">October</OPTION>
-					<OPTION value="11">November</OPTION>
-					<OPTION value="12">December</OPTION>
-				</SELECT>
-			</div>
-		</div>
-		<div class="col-xs-4">
-			<div class="input-icon">
-				<SELECT aria-required="true" name="cardyear" class="form-control lesspadding" data-stripe="exp-year">
+		<div class="col-xs-12">
+			<div class="input-group double-input">
+				<span class="input-group-addon" id="calendar-addon"><i class="fa fa-fw fa-calendar"></i></span>
+				<SELECT aria-required="true" name="cardmonth" class="form-control lesspadding" data-stripe="exp-month" aria-describedby="calendar-addon">
 					<?php
-						$current_year = date("Y");//2 digits
-						for($now = $current_year; $now < $current_year + 10; $now++){
-							echo '<OPTION VALUE="' . $now. '">' . $now . '</OPTION>';
+						$Months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+						foreach($Months as $Number => $Month){
+							$Number++;
+							if($Number < 10){$Number = "0" . $Number;}
+							echo '<OPTION value="' . $Number . '">' . $Number . ' ' . $Month . '</OPTION>';
 						}
+					?>
+				</SELECT>
+				<SELECT aria-required="true" name="cardyear" class="form-control lesspadding" data-stripe="exp-year" aria-describedby="calendar-addon">
+					<?php
+					$current_year = date("Y");//2 digits
+					for($now = $current_year; $now < $current_year + 10; $now++){
+						echo '<OPTION VALUE="' . $now. '">' . $now . '</OPTION>';
+					}
 					?>
 				</SELECT>
 			</div>
