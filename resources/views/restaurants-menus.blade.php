2fc99a7fb051edcc94a48f4f091c39c449782b4b
trino <minesh@trinoweb.com>
2016-04-05 11:29:32 -0400

    Roy 11:30

--- resources/views/restaurants-menus.blade.php
+++ resources/views/restaurants-menus.blade.php
@@ -79,8 +79,8 @@
                     </div>
                 </div>
 <?
-                    //profile is logged in and not a rest. employee and rest. is not open, or is an employee of the rest., or was uploaded by this user
-                    if( (read("id") && !read("restaurant_id") && !$restaurant->open) || $is_my_restro || $restaurant->uploaded_by == read("id") ){ ?>
+                    //is an admin, profile is logged in and not a rest. employee and rest. is not open, or is an employee of the rest., or was uploaded by this user
+                    if(read("profiletype") == 1 || (read("id") && !read("restaurant_id") && !$restaurant->open) || $is_my_restro || $restaurant->uploaded_by == read("id") ){ ?>
                     <div class="card  m-b-0" style="border-radius:0 !important;">
                         <div class="card-block ">
                             <div class="container" style="margin-top: 0 !important;padding:0 !important;">
