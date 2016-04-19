8794268c2769750ba511503871130ff056f321e5
trino <minesh@trinoweb.com>
2016-04-19 14:43:56 -0400

    Roy 2:40

--- resources/views/ajax/search_restaurants.blade.php
+++ resources/views/ajax/search_restaurants.blade.php
@@ -1,102 +1,92 @@
 <?php
-//used for getting the time in the user's time zone
-function offsettime($time, $hours = 0)
-{
-    if ($hours) {
-        $time = explode(":", $time);
-        $time[0] = $time[0] + $hours;
-        if ($time[0] < 0) {
-            $time[0] += 24;
+    //used for getting the time in the user's time zone
+    function offsettime($time, $hours = 0) {
+        if ($hours) {
+            $time = explode(":", $time);
+            $time[0] = $time[0] + $hours;
+            if ($time[0] < 0) {
+                $time[0] += 24;
+            }
+            if ($time[0] > 23) {
+                $time[0] -= 24;
+            }
+            $time = implode(":", $time);
         }
-        if ($time[0] > 23) {
-            $time[0] -= 24;
+        return $time;
+    }
+
+    if (isset($data['data'])) {
+        parse_str($data['data']);
+        parse_str($data['data'], $_POST);
+        if (debugmode()) {
+            var_dump($_POST);
         }
-        $time = implode(":", $time);
     }
-    return $time;
-}
 
-if (isset($data['data'])) {
-    parse_str($data['data']);
-    parse_str($data['data'], $_POST);
-    if (debugmode()) {
-        var_dump($_POST);
+    $server_gmt = date('Z') / 3600;
+    $user_gmt = \Session::get('session_gmt', $server_gmt);
+    $difference = $server_gmt - $user_gmt;
+    $server_time = date('H:i:s');
+    $user_time = $server_time;
+    printfile("<BR>" . $sql . "<BR>views/ajax/search_restaurants.blade.php");
+    if (is_object($count)) {
+        echo "Count should not be an object!!!";
+        return;
     }
-}
 
-$server_gmt = date('Z') / 3600;
-$user_gmt = \Session::get('session_gmt', $server_gmt);
-$difference = $server_gmt - $user_gmt;
-$server_time = date('H:i:s');
-$user_time = $server_time;//the live server's timezone is different than expected, just use the server time and be done with it
-//$user_time = date('H:i:s', strtotime(iif($difference > -1, '+') . $difference . ' hours'));//get the user's time
-if (!isset($sql)) {
-    $sql = "TIME ZONE IGNORED";//"Server GMT: " . $server_gmt . " User GMT: " . $user_gmt . " Difference: " . $difference . " hours Server Time: " . $server_time . " User Time: " . $user_time;
-}
-printfile("<BR>" . $sql . "<BR>views/ajax/search_restaurants.blade.php");
-if (is_object($count)) {
-    echo "Count should not be an object!!!";
-    return;
-}
+    echo '<div id="restuarant_bar">';
 
-echo '<div id="restuarant_bar">';
+    $totalCnt = 0;
+    $openCnt = 0;
+    $closedCnt = 0;
+    $openStr = "";
+    $closedStr = "";
 
-$totalCnt = 0;
-$openCnt = 0;
-$closedCnt = 0;
-$openStr = "";
-$closedStr = "";
+    if(isset($query) && $count > 0 && is_iterable($query)){
+        foreach($query as $value){
+            ob_start();
+            $is_open = \App\Http\Models\Restaurants::getbusinessday($value);
+            ?>
+                <div class="list-group">
+                    @include("dashboard.restaurant.restaurantpanel", array("Restaurant" => $value, "order" => true, "is_menu" => isset($is_menu)))
+                </div>
+            <?php
+            if ($is_open) {
+                $openStr .= "" . ob_get_contents();
+            } else {
+                $closedStr .= "" . ob_get_contents();
+            }
+            ob_end_clean();
+            $totalCnt++;
+            if (isset($is_open) && $is_open == 1) {
+                $openCnt++;
+            } else {
+                $closedCnt++;
+            }
+        }
+    }
 
-if(isset($query) && $count > 0 && is_iterable($query)){
-foreach($query as $value){
-ob_start();
-$is_open = \App\Http\Models\Restaurants::getbusinessday($value);
-?>
+    if ($openStr) {
+        echo $openStr;
+    }
+    if ($openStr && $closedStr) {
+        echo $closedStr;
+    } else if ($closedStr) {
+        echo $closedStr;
+    }
 
-<div class="list-group">
-    @include("dashboard.restaurant.restaurantpanel", array("Restaurant" => $value, "order" => true, "is_menu" => isset($is_menu)))
-</div>
-
-
-<?php
-if ($is_open) {
-    $openStr .= "" . ob_get_contents();
-} else {
-    $closedStr .= "" . ob_get_contents();
-}
-ob_end_clean();
-// move counter outside buffer
-$totalCnt++;
-if (isset($is_open) && $is_open == 1) {
-    $openCnt++;
-} else {
-    $closedCnt++;
-}
-}
-}
-
-if ($openStr) {
-    echo $openStr;
-}
-if ($openStr && $closedStr) {
-    echo $closedStr;
-} else if ($closedStr) {
-    echo $closedStr;
-}
-
-$alts = array(
-        "loadmore" => "Load more restaurants",
-        "loading" => "Loading..."
-);
+    $alts = array(
+            "loadmore" => "Load more restaurants",
+            "loading" => "Loading..."
+    );
 ?>
 </div>
-<!-- whats with the div? -->
 
 <script>
-            <?= "var totalCnt = " . $totalCnt . ";
+    var totalCnt = <?= '"' . $totalCnt . ";
          var openCnt = " . $openCnt . ";
          var closedCnt = " . $closedCnt . ";";
-            ?>
+    ?>
     var openCntMsg = "";
     var closedCntMsg = "";
     var spBR = "";
@@ -125,13 +115,10 @@ $alts = array(
                         class="loadMoreRestaurants btn btn-secondary-outline btn-lg btn-block m-b-1"
                         title="{{ $alts["loadmore"] }}">Load More ...
                 </button>
-                <img class="loadingbar" src="{{ asset('assets/images/loader.gif') }}" style="display: none;"
-                     alt="{{ $alts["loading"] }}"/>
+                <img class="loadingbar" src="{{ asset('assets/images/loader.gif') }}" style="display: none;" alt="{{ $alts["loading"] }}"/>
             </div>
         </div>
     @endif
     <input type="hidden" id="countTotalResult" value="{{ $count }}"/>
 </div>
-<img id='parentLoadingbar' src="{{ asset('assets/images/loader.gif') }}" style="display: none;"/><img
-        id='parentLoadingbar' src="{{ asset('assets/images/loader.gif') }}" style="display: none;"
-        alt="{{ $alts["loading"] }}"/>
+<img id='parentLoadingbar' src="{{ asset('assets/images/loader.gif') }}" style="display: none;" alt="{{ $alts["loading"] }}"/>
