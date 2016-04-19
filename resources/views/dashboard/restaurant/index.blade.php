c2505d09563c0019ccb4d5026a89496f9aa4cc9e
Van <info@trinoweb.com>
2016-04-19 13:59:50 -0400

    ui

--- resources/views/dashboard/restaurant/index.blade.php
+++ resources/views/dashboard/restaurant/index.blade.php
@@ -2,13 +2,14 @@
 @section('content')
     <meta name="_token" class="csrftoken" content="{{ csrf_token() }}"/>
     <script type="text/javascript">
-        window.showEntries = 1000;
+        window.showEntries = 100;
         window.page = 1;
         window.pageUrlLoad = "{{ url('restaurant/list/ajax') . "?" . http_build_query($_GET) }}";
     </script>
     <script src="{{ asset('assets/global/scripts/custom-datatable/blockUI.js') }}" type="text/javascript"></script>
     <!--script src="{{ asset('assets/global/scripts/custom-datatable/toastr.min.js') }}"></script-->
-    <script src="{{ asset('assets/global/scripts/custom-datatable/custom-plugin-datatable.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/global/scripts/custom-datatable/custom-plugin-datatable.js') }}"
+            type="text/javascript"></script>
     <div class="container">
         <div class="row">
             @include('layouts.includes.leftsidebar')
