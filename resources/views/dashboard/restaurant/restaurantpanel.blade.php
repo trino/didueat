6f11effd86ed045a0b44c404421c0e36acd7d05e
trino <minesh@trinoweb.com>
2016-04-21 09:50:44 -0400

    Roy 9:50

--- resources/views/dashboard/restaurant/restaurantpanel.blade.php
+++ resources/views/dashboard/restaurant/restaurantpanel.blade.php
@@ -73,18 +73,22 @@
                 $open = $Restaurant[$Day . "_open" . $key];
                 $close = $Restaurant[$Day . "_close" . $key];
                 if ($open && $close && $open != $close) {
+                    $allowbreak = true;
                     $Date = strtotime($open, time() + ($day * 86400));
                     //$MoreTime = "Opens in " . timediff($Date);
                     $MoreTime = "Opens ";
                     if ($day == 0) {
                         $MoreTime .= "today";
+                        if ($close > $open && $close < $user_time){$allowbreak = false;} //closed for today already
                     } else if ($day == 1) {
                         $MoreTime .= "tomorrow";
                     } else {
                         $MoreTime .= "in " . $day . " days";
                     }
-                    $MoreTime .= " at " . converttime($open);
-                    break;
+                    if($allowbreak) {
+                        $MoreTime .= " at " . converttime($open);
+                        break;
+                    }
                 }
             }
         } else {
