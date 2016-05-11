dcdcd60a39e5423eb6d0149dca7d149cb002c73b
trino <minesh@trinoweb.com>
2016-05-11 10:38:43 -0400

    Roy 10:35

--- resources/views/common/orderinfo.blade.php
+++ resources/views/common/orderinfo.blade.php
@@ -1,10 +1,4 @@
-<?php
-    $em=0;
-    if(isset($email_msg)){
-        $em = 1;
-    }
-?>
-<TABLE style="width:100%;<?php if($em){?>padding: 15px 0;<?php }?>">
+<TABLE style="width:100%;<?php if(isset($email_msg)){ echo 'padding: 15px 0;';} ?>">
     <?php
         printfile("views/common/orderinfo.blade.php");
         if ($order->remarks) {
@@ -13,6 +7,7 @@
         //$Data = array("Status" => $order->status);
         $Data['Order #'] = $order->guid;
 
+        if(!isset($type)){$type = "user";}
         if($type != "user"){
             $Data['Status'] = $order->status;
         }
@@ -20,40 +15,32 @@
         $Data["Type"] = iif($order->order_type == '1', "Delivery", "Pickup");
         $Data["Payment"] = iif( (isset($paid_for) && $paid_for == '1') || $order->paid, "Paid Online", "Cash on " . $Data["Type"]);
 
+        $date = new DateTime($order->order_time);//$date->format('l jS \of F Y h:i:s A');
+        $Data["On"] = $date->format(get_date_format());
 
-
-    $date = new DateTime($order->order_time);//$date->format('l jS \of F Y h:i:s A');
-    $Data["On"] = $date->format(get_date_format());
-
-    if ($order->order_till != "0000-00-00 00:00:00") {
-        if(!$order->order_till){
-            $Data["For"] = "As soon as possible";
-        }else{
-            $date = new DateTime($order->order_till);//$date->format('l jS \of F Y h:i:s A');
-            $Data["For"] = $date->format(get_date_format());
+        if ($order->order_till != "0000-00-00 00:00:00") {
+            if(!$order->order_till){
+                $Data["For"] = "As soon as possible";
+            }else{
+                $date = new DateTime($order->order_till);//$date->format('l jS \of F Y h:i:s A');
+                $Data["For"] = $date->format(get_date_format());
+            }
         }
-    }
 
-    $Data["Customer"] = $order->ordered_by;
+        $Data["Customer"] = $order->ordered_by;
 
-    if ($order->order_type == '1') {
-        if(!isset($order->prov) || !$order->prov){$order->prov = "ON";}
-        $Data['Address'] = $order->address2 . ', ' . $order->city. ' ' . $order->prov. ' ' . $order->postal_code;
-
-    }
+        if ($order->order_type == '1') {
+            if(!isset($order->prov) || !$order->prov){$order->prov = "ON";}
+            $Data['Address'] = $order->address2 . ', ' . $order->city. ' ' . $order->prov. ' ' . $order->postal_code;
+        }
 
 
-    $Data['Address Notes'] = $order->note;
-
-
+        $Data['Address Notes'] = $order->note;
         if (isset($user_detail) && is_object($user_detail)) {
             $Data["Customer"] = $user_detail->name;
             $Data["Cell Phone"] = $user_detail->phone;
             $Data["Email"] = $user_detail->email;
-
         }
-
-
 
 
         // $Data["Date format"] = get_date_format();
